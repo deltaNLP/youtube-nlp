7730f74834647f6ef2aae5f41a8e694da9cbec01
fistadev <fabiofistarol@gmail.com>
2021-08-19 18:57:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,24 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
+import streamlit.components.v1 as components
+# from gensim.summarization import summarize
+from googletrans import Translator
+import nltk
+from nltk.tokenize import word_tokenize
+import readtime
+import textstat
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import spacy
+from youtube_transcript_api import YouTubeTranscriptApi
+from sklearn.feature_extraction.text import CountVectorizer 
 
 
-# import plotly.express as px
-# import plotly.figure_factory as ff
+header = st.container()
+dataset = st.container()
+team = st.container()
 
-
-header = st.beta_container()
-dataset = st.beta_container()
-team = st.beta_container()
-footer = st.beta_container()
 
 
 @st.cache(persist=True)
@@ -21,27 +29,68 @@ def load_data(data):
     # return data
 
 
-with header:
-    st.title('Delta NLP')  # site title h1
-    st.subheader('Naturally learning stuff')
-    st.markdown("""---""")
-    image = Image.open('data/nlp.png')
-    st.image(image, caption="NLP")
-    st.text(' ')
-
-
 
 #############################
+
+with header:
+    image = Image.open('data/nlp.png')
+    st.image(image, caption="")
+
+#############################
+
+
+
+#########
+#SIDEBAR
+########
+
+
+st.sidebar.title('Delta NLP')  # site title h1
+st.sidebar.subheader('Naturally learning stuff')
+st.sidebar.markdown("""---""")
+st.sidebar.text(' ')
+
+# st.sidebar.header('Convert text :speech_balloon:')
+# nav = st.sidebar.radio('',['Transcript','English Translation','Summary', 'Measure text'])
+# nav = st.sidebar.radio('',['Measure text'])
+st.sidebar.write('')
+st.sidebar.write('')
+st.sidebar.write('')
+st.sidebar.write('')
+st.sidebar.write('')
+
+#About
+######
+expander = st.sidebar.expander('About')
+expander.write('Text')
+
+
+#Team
+######
+expander = st.sidebar.expander('Delta NLP Team')
+expander.write('[Farrukh]()')
+expander.write('[Umut]()')
+expander.write('[Deniz]()')
+expander.write('[Gabriel]()')
+expander.write('[Fabio](https://github.com/fistadev)')
+expander.write('[Nurlan]()')
+
+
+
+
+#######
+#PAGES
+######
 
 with dataset:
 
     st.markdown("")
-    st.subheader("Youtube Link")
-    st.text("Paste your link below")
+    st.markdown("<h3 style='text-align: left; color:#58A6FF;'><b>Youtube Link<b></h3>", unsafe_allow_html=True)
     st.markdown("")
 
-    st.text_input('Enter some text')
-    st.selectbox('Select', ['Transcript','English Translation','Summary'])
+    st.text_input('Paste your Youtube link here')
+    st.header('Convert text :speech_balloon:')
+    nav = st.radio('',['Transcript','English Translation','Summary', 'Measure text'])
     
 
 
@@ -52,31 +101,52 @@ with dataset:
     st.markdown("""---""") 
     st.markdown("")
 
+#-----------------------------------------
    
-##############################
-with team:
-    st.subheader('Team Delta NLP')
-    col1, col2, col3, col4, col5, col6= st.beta_columns(6)
-    with col1:
-        st.markdown('[Farrukh]()')
-    with col2:
-        st.markdown('[Umut]()')
-    with col3:
-        st.markdown('[Deniz]()')
-    with col4:
-        st.markdown('[Gabriel]()')
-    with col5:
-        st.markdown('[Fabio](https://github.com/fistadev)')
-    with col6:
-        st.markdown('[Nurlan]()')
+#MEASURE
+########
+       
+if nav == 'Measure text':
+    st.markdown("<h3 style='text-align: left; color:#58A6FF;'><b>Measure Text<b></h3>", unsafe_allow_html=True)
+    st.text('')
 
+    input_me = st.text_area("Input some text in English, and scroll down to analyze it", max_chars=5000)
 
+    if st.button('Measure'):
+        if input_me =='':
+            st.error('Please enter some text')
+        elif len(input_me) < 500:
+            st.error('Please enter a larger text')
+        else:
+            with st.spinner('Learning...'):
+                nltk.download('punkt')
+                rt = readtime.of_text(input_me)
+                tc = textstat.flesch_reading_ease(input_me)
+                tokenized_words = word_tokenize(input_me)
+                lr = len(set(tokenized_words)) / len(tokenized_words)
+                lr = round(lr,2)
+                st.text('Reading Time')
+                st.write(rt)
+                st.text('Text Complexity (score from 0 (hard to read), to 100 (easy to read))')
+                st.write(tc)
+                st.text('Use of different words (bigger number means more variety of words)')
+                st.write(lr)
+
+    st.markdown('___') 
+    
+    # components.html(
+    #                     """
+    #                     <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="This is SYNTHIA, the AI that turns words into knowledge via @lopezyse" data-url="https://share.streamlit.io/dlopezyse/synthia/main" data-hashtags="AI,Education,MachineLearning,Synthia" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    #                     """,
+    #                     )
+
+####################################
 
 # Footer
-with footer:
-    st.markdown("""---""")
-    st.markdown("")
-    st.markdown("")
-    st.markdown(
-        "If you have any questions, checkout our [documentation](https://github.com/deltaNLP/youtube-nlp) ")
-    st.text(' ')
+
+# st.markdown("""---""")
+st.markdown("")
+st.markdown("")
+st.markdown(
+    "If you have any questions, checkout our [documentation](https://github.com/deltaNLP/youtube-nlp) ")
+st.text(' ')
